e4ae09f1c2568c208edac1b72977bb78b24295a8
fragmepls <leo.oberrauch@gmail.com>
2022-10-19 21:03:56 +0200

    First Iteration

---
+++ passport-config.js
@@ -0,0 +1,27 @@
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt")
+
+async function init(passport, getUserByEmail) {
+    const authenticateUser = async(email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user == null) {
+            return done(null, false, { message: "No user with given email"} )
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: "Password incorrect"} )
+            }
+        } catch {
+            return done(e)
+        }
+    }
+
+    passport.use(new LocalStrategy( { usernameField: "email"}, authenticateUser))
+    passport.serializeUser((user, done) => { } )
+    passport.deserializeUser((id, done) => { } )
+}
+
+module.exports = init
