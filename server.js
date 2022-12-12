580815ea8dbe370bb3f658b2aee01db32140b0b4
fragmepls <leo.oberrauch@gmail.com>
2022-12-12 21:34:41 +0100

    Commit 3

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("views", path.join(__dirname, "views"));
+app.set("public", path.join(__dirname, "public"));
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
